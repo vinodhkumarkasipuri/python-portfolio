d9d6badb92d92807c3c35519dd74aef716981cbc
Vinod Kumar <vinodkumar.kasipuri@gmail.com>
2026-07-02 20:25:38 +0530

    Add Input Function notes and practice

---
+++ 01-python-fundamentals/05-input-function/input_practice.py
@@ -0,0 +1,26 @@
+# ==========================================================
+# Input_Function Practice
+# Author : Vinod Kumar
+# Topic  : Input_Function
+# ==========================================================
+
+# ==========================================================
+# Question: 1 Ask the user to enter their name.
+# ==========================================================
+
+name = input("Enter Your Name")
+
+print("Hello", name)
+
+
+# Expected Output : 
+
+# Enter your name: Vinod
+
+# Hello Vinod
+
+# ==========================================================
+# Question: 1 Ask the user to enter their name.
+# ========================================================== 
+
+
